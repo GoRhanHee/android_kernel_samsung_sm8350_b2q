457e6bcb44685aedc4c0cdc2c45b38144c9fadee
GoRhanHee <gorhanhee01@gmail.com>
2026-01-17 15:32:16 +0900

    build: change clang compiler

--- build.sh
+++ build.sh
@@ -11,27 +11,26 @@ export ANDROID_BUILD_TOP=$(pwd)
 git clone https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9  \
  toolchain/gcc/linux-x86/aarch64/aarch64-linux-android-4.9
 
-# Import clang-r383902
-git clone https://github.com/UniversalX-devs/prebuilts_clang_host_linux-x86_clang-r383902.git \
- toolchain/clang/host/linux-x86/clang-r383902
+# Import clang-r416183b
+git clone https://github.com/LineageOS/android_prebuilts_clang_kernel_linux-x86_clang-r416183b.git \
+ toolchain/clang/host/linux-x86/clang-r416183b
 
 # Setting toolchain path
-CLANG_DIR=${ANDROID_BUILD_TOP}/toolchain/clang/host/linux-x86/clang-r383902
+CLANG_DIR=${ANDROID_BUILD_TOP}/toolchain/clang/host/linux-x86/clang-r416183b
 GCC_DIR=${ANDROID_BUILD_TOP}/toolchain/gcc/linux-x86/aarch64/aarch64-linux-android-4.9
 PATH=$CLANG_DIR/bin:$CLANG_DIR/lib:$GCC_DIR/bin:$GCC_DIR/lib:$PATH
 
 # OEM Setting
 export ARCH=arm64
 export PRODUCT_NAME=b2q
-export LINKER="ld.lld"
 
 # Cooking Kernel Source
 MAKE_ARGS="
--j16 \
 LLVM=1 \
 LLVM_IAS=1 \
 ARCH=arm64 \
-CROSS_COMPILE=aarch64-linux-gnu- \
+READELF=$CLANG_DIR/bin/llvm-readelf \
+CROSS_COMPILE=$GCC_DIR/bin/aarch64-linux-gnu- \
 O=out
 "
 
