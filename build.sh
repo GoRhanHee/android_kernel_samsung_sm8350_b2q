d1ea737e1a49ad4ff5e6afbd86832fe00231130e
GoRhanHee <gorhanhee01@gmail.com>
2026-01-17 16:01:04 +0900

    build: change compiler bcz ass error

--- build.sh
+++ build.sh
@@ -7,35 +7,40 @@ git submodule init && git submodule update --remote
 export KSU=$1
 export ANDROID_BUILD_TOP=$(pwd)
 
-# Import Cross Compiler
-git clone https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9  \
- toolchain/gcc/linux-x86/aarch64/aarch64-linux-android-4.9
+# Define toolchain variables
+CLANG_DIR=$PWD/toolchain/neutron_18
+PATH=$CLANG_DIR/bin:$PATH
 
-# Import LLVM toolchain
-git clone https://github.com/proprietary-stuff/llvm-arm-toolchain-ship-10.0 \
- toolchain/llvm-arm-toolchain-ship/10.0
-
-# Setting toolchain path
-CLANG_DIR=${ANDROID_BUILD_TOP}/toolchain/llvm-arm-toolchain-ship/10.0
-GCC_DIR=${ANDROID_BUILD_TOP}/toolchain/gcc/linux-x86/aarch64/aarch64-linux-android-4.9
-PATH=$CLANG_DIR/bin:$CLANG_DIR/lib:$GCC_DIR/bin:$GCC_DIR/lib:$PATH
+# Check if toolchain exists
+if [ ! -f "$CLANG_DIR/bin/clang-18" ]; then
+    echo "-----------------------------------------------"
+    echo "Toolchain not found! Downloading..."
+    echo "-----------------------------------------------"
+    rm -rf $CLANG_DIR
+    mkdir -p $CLANG_DIR
+    pushd toolchain/neutron_18 > /dev/null
+    bash <(curl -s "https://raw.githubusercontent.com/Neutron-Toolchains/antman/main/antman") -S=05012024
+    echo "-----------------------------------------------"
+    echo "Patching toolchain..."
+    echo "-----------------------------------------------"
+    bash <(curl -s "https://raw.githubusercontent.com/Neutron-Toolchains/antman/main/antman") --patch=glibc
+    echo "-----------------------------------------------"
+    echo "Cleaning up..."
+    popd > /dev/null
+fi
 
 # OEM Setting
 export ARCH=arm64
 export PRODUCT_NAME=b2q
-CLANG_TRIPLE=aarch64-linux-gnu-
-KERNEL_MAKE_ENV="DTC_EXT=$(pwd)/tools/dtc CONFIG_BUILD_ARM64_DT_OVERLAY=y"
 
 # Cooking Kernel Source
 MAKE_ARGS="
 -j16 \
 LLVM=1 \
-LLVM_IAS=1 \
 ARCH=arm64 \
-READELF=$CLANG_DIR/bin/llvm-readelf \
-CROSS_COMPILE=$GCC_DIR/bin/aarch64-linux-gnu- \
-$KERNEL_MAKE_ENV \
-CLANG_TRIPLE=aarch64-linux-gnu- \
+CC=clang \
+CROSS_COMPILE=$CLANG_DIR/bin/llvm- \
+CLANG_TRIPLE=$CLANG_DIR/bin/aarch64-linux-gnu- \
 O=out
 "
 
