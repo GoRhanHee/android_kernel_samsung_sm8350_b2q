ca0a60520c789ae3576552e94786f95f660239b8
GoRhanHee <gorhanhee01@gmail.com>
2026-01-17 14:52:25 +0900

    build: change compiler

--- build.sh
+++ build.sh
@@ -7,27 +7,18 @@ git submodule init && git submodule update --remote
 export KSU=$1
 export ANDROID_BUILD_TOP=$(pwd)
 
-# Define toolchain variables
-CLANG_DIR=$PWD/toolchain/neutron_18
-PATH=$CLANG_DIR/bin:$PATH
+# Import Cross Compiler
+git clone https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9  \
+ toolchain/gcc/linux-x86/aarch64/aarch64-linux-android-4.9
 
-# Check if toolchain exists
-if [ ! -f "$CLANG_DIR/bin/clang-18" ]; then
-    echo "-----------------------------------------------"
-    echo "Toolchain not found! Downloading..."
-    echo "-----------------------------------------------"
-    rm -rf $CLANG_DIR
-    mkdir -p $CLANG_DIR
-    pushd toolchain/neutron_18 > /dev/null
-    bash <(curl -s "https://raw.githubusercontent.com/Neutron-Toolchains/antman/main/antman") -S=05012024
-    echo "-----------------------------------------------"
-    echo "Patching toolchain..."
-    echo "-----------------------------------------------"
-    bash <(curl -s "https://raw.githubusercontent.com/Neutron-Toolchains/antman/main/antman") --patch=glibc
-    echo "-----------------------------------------------"
-    echo "Cleaning up..."
-    popd > /dev/null
-fi
+# Import clang-r383902
+git clone https://github.com/UniversalX-devs/prebuilts_clang_host_linux-x86_clang-r383902.git \
+ toolchain/clang/host/linux-x86/clang-r383902
+
+# Setting toolchain path
+CLANG_DIR=${ANDROID_BUILD_TOP}/toolchain/clang/host/linux-x86/clang-r383902
+GCC_DIR=${ANDROID_BUILD_TOP}/toolchain/gcc/linux-x86/aarch64/aarch64-linux-android-4.9
+PATH=$CLANG_DIR/bin:$CLANG_DIR/lib:$GCC_DIR/bin:$GCC_DIR/lib:$PATH
 
 # OEM Setting
 export ARCH=arm64
@@ -40,8 +31,8 @@ MAKE_ARGS="
 LLVM=1 \
 LLVM_IAS=1 \
 ARCH=arm64 \
-CLANG_TRIPLE=aarch64-linux-gnu- \
-$KERNEL_MAKE_ENV \
+READELF=${CLANG_DIR}/bin/llvm-readelf \
+CROSS_COMPILE=${GCC_DIR}/bin/aarch64-linux-gnu- \
 O=out
 "
 
